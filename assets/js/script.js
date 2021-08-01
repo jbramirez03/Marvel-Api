191b9365d1492ab6565fb4bc1a523c85d878e26d
temptado <jason1287712@gmail.com>
2021-08-01 01:39:17 -0400

    10 comics show up on search for character

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,21 +4,26 @@ const searchButton = document.querySelector("#search-button");
 // keys to access api
 var PRIV_KEY = "b62c40680e3ea3090a2462bc3021628651c2d45f";
 var PUBLIC_KEY = "ab9297e9d4bda4ab94cb17eb9e3fe843";
+let thumbnails = document.querySelectorAll(".thumbnail");
 
 
-
-
+const displayImages = (call) => {
+  
+  for(let i = 0; i < thumbnails.length; i++){
+    thumbnails[i].setAttribute("src", call.data.results[i].images[0].path + '.jpg');
+  }
+};
 
   // function that makes api call and implements api key and hash
 function getCharacterComic (heroInput) {
 
   // you need a new ts every request                                                                                    
-  var ts = new Date().getTime();
-  var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+  const ts = new Date().getTime();
+  const hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
   // the api deals a lot in ids rather than just the strings you want to use
-  var characterName = heroInput; // wolverine                                                                             
-  var queryParams = `ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}&name=${characterName}`;
-  var url = `https://gateway.marvel.com/v1/public/characters?${queryParams}`;
+  const characterName = heroInput; // wolverine                                                                             
+  const queryParams = `ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}&name=${characterName}`;
+  const url = `https://gateway.marvel.com/v1/public/characters?${queryParams}`;
   fetch(url)
     .then(function (response) {
       return response.json();
@@ -34,7 +39,7 @@ function getCharacterComic (heroInput) {
         })
         .then (function (newdata) {
           console.log(newdata);
-
+          displayImages(newdata);
         })
         
     });
@@ -43,3 +48,4 @@ function getCharacterComic (heroInput) {
 searchButton.addEventListener("click", () =>{
   getCharacterComic(characterSearched.value);
 });
+
